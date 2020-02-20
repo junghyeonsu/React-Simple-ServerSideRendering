5f9ad75288b9b134a6633ee265ac7d71c7c66145
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-02-20 15:23:50 +0900

    리덕스

--- Boilerplate/client/src/index.js
+++ Boilerplate/client/src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+// import { Router , browserHistory } from 'react-router';
+import reducers from './reducers';
+// import routes from './routes';
+import promise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
